4fe4e5c1a1d4aa94b3107d34feb6941006fc3c59
Tejashree Modak <tejashree@uri.edu>
2017-12-20 10:49:26 -0500

    changes for gene id instead of mstrg

--- ht_transcriptome/mstrg_getfasta.sh
+++ ht_transcriptome/mstrg_getfasta.sh
@@ -14,20 +14,23 @@ module load genometools/1.5.9-foss-2016b
 F=/data3/marine_diseases_lab/tejashree/Bio_project_SRA/stringtie/test 
 echo $F
 
-#RUN_STEP=getinfo
+#RUN_STEP=mstrg_getinfo
+RUN_STEP=gene_getinfo
 #RUN_STEP=transcripts 
 #RUN_STEP=formatgtf
 #RUN_STEP=bedtools 
 #RUN_STEP=editfasta
-RUN_STEP=unique
+#RUN_STEP=unique
 
 set -e
 echo "START" $(date)
 
 #This part of code extracts those lines from the original stringtie_merged.gtf file that 
 #match the MSTRG#. It will extract all fields for that id and print into a new file in the same way as in gtf file. 
-if [ "${RUN_STEP}" == "getinfo" ] ; then
-    args=($(cat $F/mstrgs_sig.txt))
+#The output of this was written to the output file from which I deleted everything else but keep the results and saved it
+# as mstrg_sig.gtf that is fed into the second step.
+if [ "${RUN_STEP}" == "mstrg_getinfo" ] ; then
+    args=($(cat $F/gene_sig.txt))
     while read -r line
     do
         for i in ${args[@]}
@@ -39,6 +42,14 @@ if [ "${RUN_STEP}" == "getinfo" ] ; then
     done <$F/"stringtie_merged.gtf"
 fi
 
+#for getting lines that match the geneids and not mstrg
+if [ "${RUN_STEP}" == "gene_getinfo" ] ; then
+    while read name; do 
+    grep "name" $F/"stringtie_merged.gtf"
+    done <$F/"gene_sig.txt"
+fi 
+
+
 # I did this step manually on the head node since I got an error permission denied when I ran it through a script. 
 if [ "${RUN_STEP}" == "transcripts" ] ; then
     awk '$3=="transcript"' $F/mstrg_sig.gtf > $F/mstrg_sig_noexons.gtf
@@ -46,7 +57,7 @@ fi
 
 #To format gtf file for bedtools. 
 if [ "${RUN_STEP}" == "formatgtf" ] ; then
-    awk -v OFS='\t' -F'\t' '{print $1,$4,$5,$9,$6,$7}' mstrg_sig_noexon.gtf > mstrg_sig_noexon.bed 
+    awk -v OFS='\t' -F'\t' '{print $1,$4,$5,$9,$6,$7}' mstrg_sig_noexons.gtf > mstrg_sig_noexons.bed 
 fi
 
 #bedtools getfasta extracts sequences from a FASTA file for each of the intervals defined in a BED/GFF/VCF file.
@@ -56,18 +67,19 @@ fi
 #      3. Using the -name option, one can set the FASTA header for each extracted sequence to be the “name” columns from the BED feature.
 if [ "${RUN_STEP}" == "bedtools" ] ; then
     bedtools getfasta -fi /data3/marine_diseases_lab/tejashree/Bio_project_SRA/cvir_genome/cvir_edited.fa \
-                        -bed $F/mstrg_sig_noexon.bed -s -name -fo $F/mstrg_sig_noexon.fa.out 
+                        -bed $F/mstrg_sig_noexons.bed -s -name -fo $F/mstrg_sig_noexons.fa.out 
 fi
 
 #Edit the output fasta file to only keep the MSTRG#. (can also use the names.awk script to edit .bed file before running bedtools
 if [ "${RUN_STEP}" == "editfasta" ] ; then
-    awk -F '"' '{if ($1 ~ /^>/) {print ">" $2} else {print}}' mstrg_sig_noexon.fa.out > mstrg_sig_noexon.fa
+    awk -F '"' '{if ($1 ~ /^>/) {print ">" $2} else {print}}' mstrg_sig_noexons.fa.out > mstrg_sig_noexons.fa
 fi
 
 #Remove duplicate sequences with genometools
 #http://genometools.org/tools/gt_sequniq.html
-## For the gene_count significant genes: 164 out of 783 sequences have been removed (20.945%0)
+## for mstrg: For the gene_count significant genes: 164 out of 783 sequences have been removed (20.945%0)
+## for geneids: 3 out of 24 sequences have been removed (12.500%) 
 if [ "${RUN_STEP}" == "unique" ] ; then
-    gt sequniq -o $F/mstrg_sig_noexon_unique.fa $F/mstrg_sig_noexon.fa
+    gt sequniq -o $F/mstrg_sig_noexons_unique.fa $F/mstrg_sig_noexons.fa
 fi
 
