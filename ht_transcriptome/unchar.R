6f93b87aae5f6910c1c4e7835df0568b2c27c8f4
Tejashree Modak <tejashree@uri.edu>
2018-07-12 14:43:45 -0400

    fixed script to remove uppercase annot:Unchar

--- ht_transcriptome/unchar.R
+++ ht_transcriptome/unchar.R
@@ -1,18 +1,22 @@
 #This script is used to get the top 10 blast hit per transcript to get annotation for genes with 'uncharacterized' annotation
 library(plyr)
 
+setwd("/Users/tejashree/Documents/Projects/oyster/exp_data/Spring2017/transcriptome_2017/data_analysis/uncharacterized/trans_2")
+
 #The 2 csv files below are made on bluewaves by using the python script blast_extract_anno.py.
 #Instructions to generate them are in the log section "Uncharacterized".
-testx <- read.csv2("/Users/tejashree/Documents/Projects/oyster/exp_data/Spring2017/transcriptome_2017/data_analysis/uncharacterized/trans_2/S4_2vsCon_trans2_combined_annot_top10.csv", 
+testx <- read.csv2("/Users/tejashree/Documents/Projects/oyster/exp_data/Spring2017/transcriptome_2017/data_analysis/uncharacterized/trans_2/RI_2vsCon_trans2_combined_annot_top10.csv", 
                   sep=",", header=TRUE)
-testy <- read.csv2("/Users/tejashree/Documents/Projects/oyster/exp_data/Spring2017/transcriptome_2017/data_analysis/uncharacterized/trans_2/S4_2vsCon_trans2_combined_uncharac.csv", 
+testy <- read.csv2("/Users/tejashree/Documents/Projects/oyster/exp_data/Spring2017/transcriptome_2017/data_analysis/uncharacterized/trans_2/RI_2vsCon_trans2_combined_uncharac.csv", 
                    sep=",", header=TRUE)
 testnew <- match_df(testx, testy, on = "Iteration_query.def")
 
 #Get the length and unique count of ids
 sapply(testnew, function(x) length(unique(x)))
-#Remove hits that contains uncharacterized/hypothetical
+#Remove hits that contains uncharacterized/hypothetical in their annotation
 testnew_notunchar <- testnew[!grepl("uncharacterized", testnew$Hit_def),]
+sapply(testnew_notunchar, function(x) length(unique(x)))
+testnew_notunchar <- testnew_notunchar[!grepl("Uncharacterized", testnew_notunchar$Hit_def),]
 sapply(testnew_notunchar, function(x) length(unique(x)))
 testnew_notunchar <- testnew_notunchar[!grepl("hypothetical", testnew_notunchar$Hit_def),]
 sapply(testnew_notunchar, function(x) length(unique(x)))
@@ -21,5 +25,5 @@ testnew_notunchar_uniq <- testnew_notunchar[!duplicated(testnew_notunchar$Iterat
 #making sure we keep all hits and not
 sapply(testnew_notunchar_uniq, function(x) length(unique(x)))
 #write output 
-write.csv(testnew_notunchar_uniq, file = "/Users/tejashree/Documents/Projects/oyster/exp_data/Spring2017/transcriptome_2017/data_analysis/uncharacterized/trans_2/S4_2vsCon_trans2_newannot.csv",
+write.csv(testnew_notunchar_uniq, file = "RI_2vsCon_trans2_newannot.csv",
           row.names = F)
